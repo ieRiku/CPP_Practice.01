eaec9a62846ead9a5c2279d49a7246a7432ed491
Mriganka Patra <mriganka9432@gmail.com>
2024-12-19 22:37:52 +0530

    The install.sh works now perfectly, need to modify the CMakeLists.txt with proper directory placements

--- pt6-flappy-bird/src/install.sh
+++ pt6-flappy-bird/src/install.sh
@@ -1,1 +1,1 @@
-g++ main.cpp -I pak64/include -L pak64/lib -m64 -lmingw32 -lSDL2main -lSDL2
+g++ main.cpp -I ../pak64/include -L ../pak64/lib -I ../pak64-img/include -L ../pak64-img/lib -m64 -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lstdc++ -o main.exe
